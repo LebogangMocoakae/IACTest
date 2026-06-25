2f5d4b226019972bee60ee912f7afcf496f70c8a
Lebogang Mocoakae <Lebogang.Mocoakae@liquidc2.com>
2026-06-25 09:37:05 +0200

    Aprrovals1

--- Main.tf
+++ Main.tf
@@ -29,6 +29,25 @@ resource "azurerm_public_ip" "pip" {
   sku                 = "Standard" 
 }
 
+# --- NEW: Network Security Group with RDP Rule ---
+resource "azurerm_network_security_group" "nsg" {
+  name                = "my-vm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*" # Note: For safety in production, change "*" to your actual local public IP
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "my-nic"
   location            = azurerm_resource_group.rg.location
@@ -42,13 +61,19 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+# --- NEW: Connect the NSG to the NIC ---
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "my-win-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_D4s_v3" # 4 vCPUs
+  size                = "Standard_D4s_v3"
   admin_username      = "azureuser"
-  admin_password      = "P@ssw0rd1234!" # Ensure this meets complexity requirements
+  admin_password      = "P@ssw0rd1234!" 
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
